c71fde7d625fe78221f20b6dbc7739df9eabef8e
Jiayang Zhang <fantasy090633@gmail.com>
2014-08-14 20:45:30 +0800

    8月14日新刷新日期

--- gen_yaml.py
+++ gen_yaml.py
@@ -5,9 +5,9 @@ import random
 # 配置
 day_map = {
     # 开始日期
-    'start_date': datetime.datetime.strptime('8-9', '%m-%d'),
+    'start_date': datetime.datetime.strptime('8-15', '%m-%d'),
     # 0表示不刷1表示刷，从开始日期开始上下午循环
-    'is_vote': '010101010111',
+    'is_vote': '10101010101010000101010101',
 }
 
 # 上午下午上班开始时间
